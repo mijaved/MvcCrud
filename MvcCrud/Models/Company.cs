20080cda5fc5d9c18192a18e9a512839491a1518
mijaved <mijaved@gmail.com>
2018-02-08 19:04:43 +0600

    updated

--- MvcCrud/Models/Company.cs
+++ MvcCrud/Models/Company.cs
@@ -18,7 +18,7 @@ namespace MvcCrud.Models
             ID = 1;
             Name = "TVL";
             Address = "Uttara, Dhaka";
-            EmployeeList = GetEmployees(); //new List<Employee>();
+            EmployeeList = new Employee().GetEmployees(); //new List<Employee>();
         }
 
         #region CRUD
@@ -32,33 +32,14 @@ namespace MvcCrud.Models
                     ID = i + 1,
                     Name = "TVL " + i.ToString(),
                     Address = "Uttara, Dhaka - " + i.ToString(),
-                    EmployeeList = GetEmployees()
+                    EmployeeList = new Employee().GetEmployees()
                 };
                 list.Add(model);
             }
             return list;
         }
 
-        private List<Employee> GetEmployees()
-        {
-            var empList = new List<Employee>();
-            Employee emp = null;
-            for (int i = 0; i < 5; i++)
-            {
-                emp = new Employee()
-                {
-                    CompanyID = 1,
-                    EmployeeID = 1 + i,
-                    Name = "Name " + i.ToString(),
-                    Email = "Email " + i.ToString(),
-                    Phone = "Phone " + i.ToString(),
-                    Position = "Position " + i.ToString()
-                };
-
-                empList.Add(emp);
-            }
-            return empList;
-        }
+        
 
         public Company GetCompany(int ID)
         {
--- MvcCrud/Models/Employee.cs
+++ MvcCrud/Models/Employee.cs
@@ -13,5 +13,26 @@ namespace MvcCrud.Models
         public string Phone { get; set; }
         public string Email { get; set; }
         public int CompanyID { get; set; }
+
+        public List<Employee> GetEmployees()
+        {
+            var empList = new List<Employee>();
+            Employee emp = null;
+            for (int i = 0; i < 5; i++)
+            {
+                emp = new Employee()
+                {
+                    CompanyID = 1,
+                    EmployeeID = 1 + i,
+                    Name = "Name " + i.ToString(),
+                    Email = "Email " + i.ToString(),
+                    Phone = "Phone " + i.ToString(),
+                    Position = "Position " + i.ToString()
+                };
+
+                empList.Add(emp);
+            }
+            return empList;
+        }
     }
 }
